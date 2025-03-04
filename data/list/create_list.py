e96f5224edcdb0051b3fcee5dcd7f3d2a1ba2429
Zeno Kujawa <30478937+ZenoMK@users.noreply.github.com>
2025-03-04 13:16:09 -0500

    lists variable length

--- data/list/create_list.py
+++ data/list/create_list.py
@@ -5,7 +5,7 @@ import argparse
 def generate_random_list(num_nodes):
     """Generate a random list of integers with length between 5 and 100,
        followed by '%' and the reversed list."""
-    length = random.randint(num_nodes)
+    length = random.randint(2, num_nodes)
     rand_list = [random.randint(0, num_nodes - 1) for _ in range(length)]
     #rand_list = sorted(rand_list)# Generate random integers (0-1000)
     reversed_list = list(reversed(rand_list))
